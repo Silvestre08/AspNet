a7ec8e9d58bef042f711166848afe1bb169f8f49
Celso <celsosilvestre08@gmail.com>
2024-07-07 08:06:07 -0400

    add migrations EF core

--- WebApi/WebApiFundamentals/CityInfo/CityInfo.Api/DbContexts/CityInfoContext.cs
+++ WebApi/WebApiFundamentals/CityInfo/CityInfo.Api/DbContexts/CityInfoContext.cs
@@ -6,6 +6,11 @@ namespace CityInfo.Api.DbContexts
 {
     public class CityInfoContext : DbContext
     {
+        public CityInfoContext(DbContextOptions<CityInfoContext> options) 
+            : base(options)
+        {
+        }
+
         public DbSet<City> Cities { get; set; }
 
         public DbSet<PointOfInterest> PointsOfInterest { get; set; }
