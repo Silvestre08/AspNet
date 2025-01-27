e033413819c2e031e4a79d9427266c77567354e5
Celso <celsosilvestre08@gmail.com>
2025-01-26 21:29:22 -0500

    authorization handlers

--- WebApi/OAuth2AndOpenId/ImageGallery.Client/Controllers/GalleryController.cs
+++ WebApi/OAuth2AndOpenId/ImageGallery.Client/Controllers/GalleryController.cs
@@ -128,7 +128,8 @@ namespace ImageGallery.Client.Controllers
             return RedirectToAction("Index");
         }
 
-        [Authorize(Roles = "PayingUser")]
+        //[Authorize(Roles = "PayingUser")]
+        [Authorize(Policy = "UserCanAddImage")]
         public IActionResult AddImage()
         {
             return View();
@@ -136,7 +137,8 @@ namespace ImageGallery.Client.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        [Authorize(Roles = "PayingUser")]
+        [Authorize(Policy = "UserCanAddImage")]
+        //[Authorize(Roles = "PayingUser")]
         public async Task<IActionResult> AddImage(AddImageViewModel addImageViewModel)
         {
             if (!ModelState.IsValid)
--- WebApi/OAuth2AndOpenId/Marvin.IDP/Config.cs
+++ WebApi/OAuth2AndOpenId/Marvin.IDP/Config.cs
@@ -11,20 +11,23 @@ public static class Config
             new IdentityResources.OpenId(),
             new IdentityResources.Profile(),
             new IdentityResource("roles", "Your role(s)", new [] { "role" }),
+            new IdentityResource("country", "The country you are living in", new [] { "country" }),
         };
 
     public static IEnumerable<ApiScope> ApiScopes =>
         new ApiScope[]
             {
-                new ApiScope("imagegalleryapi.fullaccess")
+                new ApiScope("imagegalleryapi.fullaccess"),
+                new ApiScope("imagegalleryapi.read"),
+                new ApiScope("imagegalleryapi.write")
             };
 
     public static IEnumerable<ApiResource> ApiResources =>
     new ApiResource[]
     {
-        new ApiResource("imagegalleryapi", "Image Gallerey API", new []{ "role" })
+        new ApiResource("imagegalleryapi", "Image Gallerey API", new []{ "role", "country"})
         {
-            Scopes = { "imagegalleryapi.fullaccess" }
+            Scopes = { "imagegalleryapi.fullaccess", "imagegalleryapi.read", "imagegalleryapi.write" }
         }
     };
 
@@ -40,12 +43,13 @@ public static class Config
                     IdentityServerConstants.StandardScopes.OpenId,
                     IdentityServerConstants.StandardScopes.Profile,
                     "roles",
-                    "imagegalleryapi.fullaccess"
+                    //"imagegalleryapi.fullaccess",
+                    "imagegalleryapi.read",
+                    "imagegalleryapi.write",
+                    "country"
                 },
                 ClientSecrets = { new Secret("secret".Sha256()) },
-                RequireConsent = true,
-                
+                RequireConsent = true,    
             }
-
             };
 }
