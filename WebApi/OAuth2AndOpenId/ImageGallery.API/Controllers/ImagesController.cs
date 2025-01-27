54f461c9270591340619e896fc7c9ab98ddf18c8
Celso <celsosilvestre08@gmail.com>
2025-01-26 21:35:09 -0500

    authorize attribute

--- WebApi/OAuth2AndOpenId/ImageGallery.API/Controllers/ImagesController.cs
+++ WebApi/OAuth2AndOpenId/ImageGallery.API/Controllers/ImagesController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using ImageGallery.API.Authorization;
 using ImageGallery.API.Services;
 using ImageGallery.Model;
 using Microsoft.AspNetCore.Authorization;
@@ -48,7 +49,8 @@ namespace ImageGallery.API.Controllers
         }
 
         [HttpGet("{id}", Name = "GetImage")]
-        [Authorize("MustOwnImage")]
+        //[Authorize("MustOwnImage")]
+        [MustOwnImage]
         public async Task<ActionResult<Image>> GetImage(Guid id)
         {          
             var imageFromRepo = await _galleryRepository.GetImageAsync(id);
