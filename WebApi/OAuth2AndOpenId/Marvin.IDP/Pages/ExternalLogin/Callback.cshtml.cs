7a19ba3182acf62da9c36b8e0c3f8fc902ac6f71
Celso Silvestre <csilvestre@rapidmicrobio.com>
2025-03-19 22:02:30 -0400

    integrating with Azure AD

--- WebApi/OAuth2AndOpenId/Marvin.IDP/Pages/ExternalLogin/Callback.cshtml.cs
+++ WebApi/OAuth2AndOpenId/Marvin.IDP/Pages/ExternalLogin/Callback.cshtml.cs
@@ -18,7 +18,6 @@ namespace Marvin.IDP.Pages.ExternalLogin;
 [SecurityHeaders]
 public class Callback : PageModel
 {
-    private readonly TestUserStore _users;
     private readonly IIdentityServerInteractionService _interaction;
     private readonly ILogger<Callback> _logger;
     private readonly IEventService _events;
@@ -26,12 +25,8 @@ public class Callback : PageModel
     public Callback(
         IIdentityServerInteractionService interaction,
         IEventService events,
-        ILogger<Callback> logger,
-        TestUserStore? users = null)
+        ILogger<Callback> logger)
     {
-        // this is where you would plug in your own custom identity management library (e.g. ASP.NET Identity)
-        _users = users ?? throw new InvalidOperationException("Please call 'AddTestUsers(TestUsers.Users)' on the IIdentityServerBuilder in Startup or remove the TestUserStore from the AccountController.");
-
         _interaction = interaction;
         _logger = logger;
         _events = events;
@@ -67,18 +62,18 @@ public class Callback : PageModel
         var providerUserId = userIdClaim.Value;
 
         // find external user
-        var user = _users.FindByExternalProvider(provider, providerUserId);
-        if (user == null)
-        {
-            // this might be where you might initiate a custom workflow for user registration
-            // in this sample we don't show how that would be done, as our sample implementation
-            // simply auto-provisions new external user
-            //
-            // remove the user id claim so we don't include it as an extra claim if/when we provision the user
-            var claims = externalUser.Claims.ToList();
-            claims.Remove(userIdClaim);
-            user = _users.AutoProvisionUser(provider, providerUserId, claims.ToList());
-        }
+        //var user = _users.FindByExternalProvider(provider, providerUserId);
+        //if (user == null)
+        //{
+        //    // this might be where you might initiate a custom workflow for user registration
+        //    // in this sample we don't show how that would be done, as our sample implementation
+        //    // simply auto-provisions new external user
+        //    //
+        //    // remove the user id claim so we don't include it as an extra claim if/when we provision the user
+        //    var claims = externalUser.Claims.ToList();
+        //    claims.Remove(userIdClaim);
+        //    user = _users.AutoProvisionUser(provider, providerUserId, claims.ToList());
+        //}
 
         // this allows us to collect any additional claims or properties
         // for the specific protocols used and store them in the local auth cookie.
@@ -88,9 +83,9 @@ public class Callback : PageModel
         CaptureExternalLoginContext(result, additionalLocalClaims, localSignInProps);
             
         // issue authentication cookie for user
-        var isuser = new IdentityServerUser(user.SubjectId)
+        var isuser = new IdentityServerUser(providerUserId)
         {
-            DisplayName = user.Username,
+            DisplayName = providerUserId,
             IdentityProvider = provider,
             AdditionalClaims = additionalLocalClaims
         };
@@ -105,7 +100,7 @@ public class Callback : PageModel
 
         // check if external login is in the context of an OIDC request
         var context = await _interaction.GetAuthorizationContextAsync(returnUrl);
-        await _events.RaiseAsync(new UserLoginSuccessEvent(provider, providerUserId, user.SubjectId, user.Username, true, context?.Client.ClientId));
+        await _events.RaiseAsync(new UserLoginSuccessEvent(provider, providerUserId, providerUserId, providerUserId, true, context?.Client.ClientId));
         Telemetry.Metrics.UserLogin(context?.Client.ClientId, provider!);
 
         if (context != null)
